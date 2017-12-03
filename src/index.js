8df97fce42088d6571b263f007c778928db215a9
Josh Duck <github@joshduck.com>
2017-12-03 20:53:33 +1000

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -7,43 +7,50 @@ import {
 } from "./util/drawing";
 import { makeWorld } from "./world";
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-const world = makeWorld();
-
-const moveNodes = entities => {
+const updateEntities = (entities, targets) => {
   for (let i = 0; i < entities.length; i++) {
     entities[i].updatePosition();
   }
-};
 
-const attractNodes = (entities, targets) => {
   for (let i = 0; i < entities.length; i++) {
     entities[i].updateVelocity(entities, targets);
   }
 };
 
-const step = () => {
+const step = (ctx, world) => {
   const { entities, targets, bounds } = world;
-  attractNodes(entities, targets);
-  moveNodes(entities);
+
+  // Movement
+  updateEntities(entities, targets);
+
+  // Drawing
   fillBounds(ctx, bounds);
   drawTargets(ctx, targets);
   drawEntities(ctx, entities);
   //drawEntityForces(ctx, entities);
-  requestAnimationFrame(step);
+
+  requestAnimationFrame(() => step(ctx, world));
 };
 
-window.onmousemove = e => {
-  world.touchPoint.position.x = e.offsetX;
-  world.touchPoint.position.y = e.offsetY;
-  //console.log(e);
+const initialize = canvas => {
+  const ctx = canvas.getContext("2d");
+  const world = makeWorld();
+
+  // Avoid touches
+  canvas.addEventListener("mousemove", e => {
+    world.touchPoint.position.x = e.offsetX;
+    world.touchPoint.position.y = e.offsetY;
+  });
+
+  // Randomness to the flow
+  setInterval(() => {
+    world.homingBeacon.position.x = random(0.25, 0.75) * world.bounds.x;
+    world.homingBeacon.position.y = random(0.25, 0.75) * world.bounds.y;
+  }, 5000);
+
+  // Go, go, go!
+  step(ctx, world);
 };
 
-step();
-setInterval(() => {
-  world.homingBeacon.position.x = random(0.25, 0.75) * world.bounds.x;
-  world.homingBeacon.position.y = random(0.25, 0.75) * world.bounds.y;
-  //  run = !run;
-  //  step();
-}, 5000);
+var canvas = document.getElementById("canvas");
+initialize(canvas);
