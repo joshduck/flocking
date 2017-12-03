d0ca6497cb2c690a12b5d16ee49f6daed5a9b8d2
Josh Duck <github@joshduck.com>
2017-12-03 20:48:39 +1000

    Move drawing to module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import convert from "color-convert";
 import random from "./util/random";
+import {
+  drawEntities,
+  drawTargets,
+  drawEntityForces,
+  fillBounds
+} from "./util/drawing";
 import { makeWorld } from "./world";
 
 var canvas = document.getElementById("canvas");
@@ -12,63 +17,20 @@ const moveNodes = entities => {
   }
 };
 
-const drawFill = () => {
-  ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
-  ctx.fillRect(0, 0, world.bounds.x, world.bounds.y);
-};
-
-const drawNodes = entities => {
-  for (let i = 0; i < entities.length; i++) {
-    const { position, velocity, attraction, correlation, target } = entities[i];
-
-    // ctx.strokeStyle = "rgba(0, 255, 0, 0.5)";
-    // ctx.beginPath();
-    // ctx.moveTo(position.x, position.y);
-    // ctx.lineTo(position.x + target.x, position.y + target.y);
-    // ctx.stroke();
-
-    // ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    // ctx.beginPath();
-    // ctx.moveTo(position.x, position.y);
-    // ctx.lineTo(position.x + attraction.x, position.y + attraction.y);
-    // ctx.stroke();
-
-    // ctx.strokeStyle = "rgba(255, 0, 0, 0.5)";
-    // ctx.beginPath();
-    // ctx.moveTo(position.x, position.y);
-    // ctx.lineTo(position.x + correlation.x, position.y + correlation.y);
-    // ctx.stroke();
-
-    ctx.strokeStyle =
-      "#" + convert.hsl.hex((entities[i].speed * 512) % 255, 100, 50);
-    ctx.beginPath();
-    ctx.moveTo(position.x, position.y);
-    ctx.lineTo(position.x - velocity.x, position.y - velocity.y);
-    ctx.stroke();
-  }
-};
-
-const drawTargets = targets => {
-  for (let i = 0; i < targets.length; i++) {
-    ctx.fillStyle = "#000";
-    ctx.fillRect(targets[i].position.x - 2, targets[i].position.y - 2, 4, 4);
-  }
-};
-
 const attractNodes = (entities, targets) => {
   for (let i = 0; i < entities.length; i++) {
     entities[i].updateVelocity(entities, targets);
   }
 };
 
-console.log(world);
 const step = () => {
-  const { entities, targets } = world;
+  const { entities, targets, bounds } = world;
   attractNodes(entities, targets);
   moveNodes(entities);
-  drawFill();
-  drawTargets(targets);
-  drawNodes(entities);
+  fillBounds(ctx, bounds);
+  drawTargets(ctx, targets);
+  drawEntities(ctx, entities);
+  //drawEntityForces(ctx, entities);
   requestAnimationFrame(step);
 };
 
---
+++ src/util/drawing.js
@@ -0,0 +1,54 @@
+import convert from "color-convert";
+
+export const fillBounds = (ctx, bounds) => {
+  ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+  ctx.fillRect(0, 0, bounds.x, bounds.y);
+};
+
+export const drawVector = (ctx, position, offset, multiplier) => {
+  ctx.beginPath();
+  ctx.moveTo(position.x, position.y);
+  ctx.lineTo(
+    position.x + offset.x * multiplier,
+    position.y + offset.y * multiplier
+  );
+  ctx.stroke();
+};
+
+export const drawEntityForces = (ctx, entities) => {
+  for (let i = 0; i < entities.length; i++) {
+    const { position, attraction, correlation, target } = entities[i];
+
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = "red";
+    drawVector(ctx, position, target, 10);
+
+    ctx.strokeStyle = "green";
+    drawVector(ctx, position, attraction, 10);
+
+    ctx.strokeStyle = "blue";
+    drawVector(ctx, position, correlation, 10);
+  }
+};
+
+export const drawEntities = (ctx, entities) => {
+  for (let i = 0; i < entities.length; i++) {
+    const { position, velocity } = entities[i];
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle =
+      "#" + convert.hsl.hex((entities[i].speed * 512) % 255, 100, 50);
+    drawVector(ctx, position, velocity, -1);
+  }
+};
+
+export const drawTargets = (ctx, targets) => {
+  for (let i = 0; i < targets.length; i++) {
+    const { position } = targets[i];
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(position.x, position.y, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+};
